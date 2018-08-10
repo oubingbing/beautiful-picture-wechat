c7eb4730dfa3db6dab1f01cd941af284079bb4b5
ouzhibing <ouzhibing@galaxymx.com>
2018-08-10 17:49:26 +0800

    初始化项目

---
+++ pages/index/index.js
@@ -0,0 +1,76 @@
+
+const util = require("../../utils/util.js");
+const qiniuUploader = require("../../utils/qiniuUploader");
+const uploader = require("../../utils/uploadImage");
+
+const app = getApp()
+
+wx.onUserCaptureScreen(function (res) {
+  console.log("res：" + JSON.stringify(res));
+})
+
+Page({
+  data: {
+    show_auth:app.globalData.show_auth,
+    userInfo: {}
+  },
+
+  onLoad: function (e) {
+    let that = this;
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userInfo']) {
+          that.setData({
+            show_auth:true
+          });
+        }
+      }
+    })
+
+  },
+  
+  onShow: function (option) {
+
+  },
+  /**
+   * 监听用户点击授权按钮
+   */
+  getAuthUserInfo:function(data){
+    app.globalData.show_auth = false;
+    this.setData({
+      show_auth:false
+    });
+
+    let _this = this;
+    app.login(null, null, null, function(){
+      _this.getPost(_this);
+      _this.topic();
+      console.log('加载信息');
+    });
+  },
+
+
+  /**
+ * 预览图片
+ */
+  previewMoreImage: function (event) {
+
+    console.log(event.target.id);
+    console.log(event.currentTarget.dataset.obj);
+
+    let _this = this;
+
+    let images = event.currentTarget.dataset.obj.map(item=>{
+      return _this.data.baseImageUrl+item;
+    });
+
+    console.log(images);
+
+    let url = event.target.id;
+
+    wx.previewImage({
+      current: url,
+      urls: images
+    })
+  }
+})
